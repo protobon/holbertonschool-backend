606bd6701450d62529f3c6b4e357ed538e8c7350
ayrton-hbtn <3282@holbertonschool.com>
2022-05-11 02:02:00 -0300

    LRU cache algorithm

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -1,45 +1,51 @@
 #!/usr/bin/env python3
-""""""
+"""Caching algorithms"""
 
 BaseCaching = __import__('base_caching').BaseCaching
 
 
 class LRUCache(BaseCaching):
-    """"""
+    """Cache with Least Recently Used algorithm"""
     use_count = {}
-    keys_order = []
 
     def __init__(self):
+        """inits from BaseCaching"""
         super().__init__()
 
     def put(self, key, item):
-        """"""
+        """
+        Puts a new item to cache key.
+
+        An auxiliar queue structure takes
+        count of the keys that are being used.
+        If the number of items reach a limit
+        set, then the key that hasn't been used
+        for the most time gets removed (LRU).
+        """
         if not key or not item:
             return
-        if key in self.cache_data:
-            self.cache_data[key] = item
-            self.use_count[key] += 1
-            self.keys_order.append((key, self.use_count[key]))
-            return
-        if len(self.cache_data) == BaseCaching.MAX_ITEMS:
-            lru = sorted(self.keys_order, key=lambda tup: tup[1])
-            while True:
-                del_key = lru.pop(0)[0]
-                if del_key in self.cache_data.keys():
-                    break
-            del self.cache_data[del_key]
-            print(f"DISCARD: {del_key}")
-        if key not in self.use_count:
-            self.use_count[key] = 0
-        else:
-            self.use_count[key] += 1
-        self.keys_order.append((key, self.use_count[key]))
+        if key not in self.cache_data.keys():
+            if len(self.cache_data) == BaseCaching.MAX_ITEMS:
+                del_key = list(self.use_count.keys())[0]
+                print(f"DISCARD: {del_key}")
+                del self.cache_data[del_key]
+                del self.use_count[del_key]
+
+        if key in self.use_count.keys():
+            del self.use_count[key]
+        self.use_count[key] = 1
         self.cache_data[key] = item
 
     def get(self, key):
-        """"""
+        """
+        Retrieves value from cache key.
+
+        Everytime a key is retrieved, it is
+        added to the auxiliar queue structure
+        of Least Recently Used (to the last place).
+        """
         if not key or key not in self.cache_data:
             return None
-        self.use_count[key] += 1
-        self.keys_order.append((key, self.use_count[key]))
+        del self.use_count[key]
+        self.use_count[key] = 1
         return self.cache_data[key]
