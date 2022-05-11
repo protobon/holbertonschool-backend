2313a87ba41d80090dd9f042abfc1373093ce55c
ayrton-hbtn <3282@holbertonschool.com>
2022-05-11 02:01:40 -0300

    cache algorithm with LIFO sequence

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -1,19 +1,25 @@
 #!/usr/bin/env python3
-""""""
+"""Caching Algorithms"""
 
 BaseCaching = __import__('base_caching').BaseCaching
 
 
 class LIFOCache(BaseCaching):
-    """"""
+    """Cache with LIFO (stack) structure"""
     keys_order = []
 
     def __init__(self):
-        """"""
+        """init from BaseCaching"""
         super().__init__()
-    
+
     def put(self, key, item):
-        """"""
+        """
+        puts a new item to Cache key
+
+        if number of items reach a limit set
+        then the newest item is removed to make space
+        (Last in, First out)
+        """
         if not key or not item:
             return
         if key in self.cache_data:
@@ -28,7 +34,7 @@ class LIFOCache(BaseCaching):
         self.keys_order.append(key)
 
     def get(self, key):
-        """"""
+        """retrieves value from Cache key"""
         if not key or key not in self.cache_data:
             return None
         return self.cache_data[key]
