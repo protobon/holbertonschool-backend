9e181514904b841cd8034fb8442714d7d2a3ebe6
ayrton-hbtn <3282@holbertonschool.com>
2022-06-15 01:22:44 -0300

    fix import

--- 0x03-queuing_system_in_js/0-redis_client.js
+++ 0x03-queuing_system_in_js/0-redis_client.js
@@ -1,14 +1,14 @@
-import { createClient } from 'redis';
+import * as redis from 'redis';
 
-const client = createClient({
+const client = redis.createClient({
     url: 'redis://127.0.0.1:6379'
-    })
+    });
 
 client.on('connect', () => {
     console.log('Redis client connected to the server');
 });
 client.on('error', (err) => {
-    console.log(`Redis client not connected to the server: ${err}`);
+    console.error(`Redis client not connected to the server: ${err.message}`);
 });
 
 (async () => {
