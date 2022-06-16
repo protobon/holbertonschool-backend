8c6607fc0a8ea3b7af8297af849279b5767b7d40
ayrton-hbtn <3282@holbertonschool.com>
2022-06-15 23:53:32 -0300

    process multiple jobs

---
+++ 0x03-queuing_system_in_js/7-job_creator.js
@@ -0,0 +1,63 @@
+const kue = require('kue');
+
+const jobs = [
+    {
+      phoneNumber: '4153518780',
+      message: 'This is the code 1234 to verify your account'
+    },
+    {
+      phoneNumber: '4153518781',
+      message: 'This is the code 4562 to verify your account'
+    },
+    {
+      phoneNumber: '4153518743',
+      message: 'This is the code 4321 to verify your account'
+    },
+    {
+      phoneNumber: '4153538781',
+      message: 'This is the code 4562 to verify your account'
+    },
+    {
+      phoneNumber: '4153118782',
+      message: 'This is the code 4321 to verify your account'
+    },
+    {
+      phoneNumber: '4153718781',
+      message: 'This is the code 4562 to verify your account'
+    },
+    {
+      phoneNumber: '4159518782',
+      message: 'This is the code 4321 to verify your account'
+    },
+    {
+      phoneNumber: '4158718781',
+      message: 'This is the code 4562 to verify your account'
+    },
+    {
+      phoneNumber: '4153818782',
+      message: 'This is the code 4321 to verify your account'
+    },
+    {
+      phoneNumber: '4154318781',
+      message: 'This is the code 4562 to verify your account'
+    },
+    {
+      phoneNumber: '4151218782',
+      message: 'This is the code 4321 to verify your account'
+    }
+  ];
+
+const queue = kue.createQueue();
+
+const job = queue.create('push_notification_code_2', {
+    "phoneNumber": "099999991",
+    "message": "Full perspective",
+  }).save( function(err){
+   err ? console.log(`Notification job ${job.id} failed: ${job.error}`) :
+   console.log(`Notification job created: ${job.id}`);
+});
+
+
+job.on('complete', function(result) {
+    console.log(`Notification job ${job.id} completed`);
+});
